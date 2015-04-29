4f1b14a24438baed7954525c43a6f1bc7e4184bf
EugeneShishkin <EugeneShishkin@users.noreply.github.com>
2015-04-28 22:34:13 -0400

    new method

--- AcumenCodeExercise/AcumenCodeExercise/Controllers/DefaultController.cs
+++ AcumenCodeExercise/AcumenCodeExercise/Controllers/DefaultController.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace AcumenCodeExercise.Controllers
 {
     public class DefaultController : ApiController
     {
+        public ActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
